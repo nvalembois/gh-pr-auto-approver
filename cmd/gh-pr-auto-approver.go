b46bf6a30be0dcba866b68281d53953a05e06591
nvalembois <nvalembois@live.com>
2024-01-21 19:18:52 +0100

    [gh-pr-auto-approver] update dependecy
    * [gh-pr-auto-approver] update dependecy

--- cmd/gh-pr-auto-approver.go
+++ cmd/gh-pr-auto-approver.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/google/go-github/v57/github"
+	"github.com/google/go-github/v58/github"
 	"github.com/sirupsen/logrus"
 
 	"github.com/nvalembois/gh-pr-auto-approver/internal"
